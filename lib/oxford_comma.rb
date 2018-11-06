6e5cbc91dd5f7b9c0116e8f2561e686ffb0251b6
a lan <bluenowte97@gmail.com>
2018-11-06 18:37:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length = 2
     array.insert(-2,"and")
     string = array.join(" ")
-  end
+  
   elsif array.length > 2
     array.insert(-2,"and")
     string = array.join(",")
