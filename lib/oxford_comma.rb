ec9135e4c495f08c2748bdfa5bd243cd9e6c2d2c
a lan <bluenowte97@gmail.com>
2018-11-06 18:36:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
-if array.length >1
-  array.insert(-2,"and")
-end
-  string = array.join(" ")
+  if array.length = 2
+    array.insert(-2,"and")
+    string = array.join(" ")
+  end
+  elsif array.length > 2
+    array.insert(-2,"and")
+    string = array.join(",")
+  end
 end
